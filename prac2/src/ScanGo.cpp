4f88e1317b6ade0d3f3b92d0f642422217241257
gbernalr <78974537+gbernalr@users.noreply.github.com>
2021-02-27 19:58:18 +0100

    Añadido giro aleatorio y dependiendo del angulo

---
+++ prac2/src/ScanGo.cpp
@@ -0,0 +1,80 @@
+#include "ros/ros.h"
+#include "ScanGo.hpp"
+#include "geometry_msgs/Twist.h"
+#include "sensor_msgs/LaserScan.h"
+#include <random>
+
+#define TURNING_TIME 5.0
+#define BACKING_TIME 3.0
+
+void ScanGo::scanCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
+{
+    int sz = msg->ranges.size();
+    int diff_turn = (int)(M_PI/5)/-msg->angle_increment;
+    detected_ = NO_TURN;
+    if (state_ == GOING_FORWARD)
+    {
+      if (msg->ranges[sz/2 - diff_turn] <= DIST_MIN)
+      {
+        detected_ = LEFT;
+      }
+      else if (msg->ranges[sz/2] <= DIST_MIN)
+      {
+        detected_ = CENTER;
+      }
+      else if (msg->ranges[sz/2 + diff_turn] <= DIST_MIN)
+      {
+        detected_ = RIGHT;
+      }
+    }
+}
+
+void ScanGo::step()
+{
+    geometry_msgs::Twist cmd;
+    srand(time(NULL));
+
+    switch (state_)
+    {
+    case GOING_FORWARD:
+      cmd.linear.x = 0.25;
+      cmd.angular.z = 0;
+      if (detected_)
+      {
+        ROS_INFO("DETECTED: %d", detected_);
+        press_ts_ = ros::Time::now();
+        state_ = GOING_BACK;
+        ROS_INFO("GOING_FORWARD -> GOING_BACK");
+      }
+      break;
+
+    case GOING_BACK:
+      cmd.linear.x = -0.25;
+      cmd.angular.z = 0;
+
+      if ((ros::Time::now() - press_ts_).toSec() > BACKING_TIME )
+      {
+        turn_ts_ = ros::Time::now();
+        state_ = TURNING;
+        ROS_INFO("GOING_BACK -> TURNING");
+      }
+      break;
+
+    case TURNING:
+      cmd.linear.x = 0;
+      // rand % (max_number +1 - minimun_number ) + mininum_number
+      cmd.angular.z = ((double)rand() / RAND_MAX) * (M_PI/2 - M_PI / 4) + M_PI / 4;
+      if (detected_ == LEFT)
+      {
+        cmd.angular.z *= -1;
+      }
+      if ((ros::Time::now()-turn_ts_).toSec() > TURNING_TIME )
+      {
+        state_ = GOING_FORWARD;
+        ROS_INFO("TURNING -> GOING_FORWARD");
+      }
+      break;
+    }
+
+    pub_.publish(cmd);
+}
